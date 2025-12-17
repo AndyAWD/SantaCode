ee1a99fb4c95c706e2b32f28df36730b21a04d50
AndyAWD <anandydy529@gmail.com>
2025-12-18 01:36:38 +0800

    feat: 增加Andy的聖誕樹

---
+++ submissions/AndyAWD/tree.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("     *")
+    println("    /|\\")
+    println("   /*|*\\")
+    println("  /|*|*|*\\")
+    println(" /|*|*|*|*\\")
+    println("/|*|*|*|*|*\\")
+    println("    | | |")
+    println("[GDG Kaohsiung by Andy 聖誕快樂！]")
+}
